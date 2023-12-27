dba1469159e1cabbbc6035945f281f37aa859c64
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2023-12-27 14:59:25 +0700

    update donasi

--- config/table-fields.php
+++ config/table-fields.php
@@ -13,34 +13,34 @@ return [
             'label' => 'Nama Lengkap',
             'type'  => 'text'
         ],
-        'jenis_kelamin' => [
-            'label' => 'Jenis Kelamin',
-            'type'  => 'text'
-        ],
-        'tanggal_lahir' => [
-            'label' => 'Tanggal Lahir',
-            'type'  => 'text'
-        ],
+        // 'jenis_kelamin' => [
+        //     'label' => 'Jenis Kelamin',
+        //     'type'  => 'text'
+        // ],
+        // 'tanggal_lahir' => [
+        //     'label' => 'Tanggal Lahir',
+        //     'type'  => 'text'
+        // ],
         'no_telepon' => [
             'label' => 'No. WA',
             'type'  => 'text'
         ],
-        'email' => [
-            'label' => 'No. WA',
-            'type'  => 'text'
-        ],
+        // 'email' => [
+        //     'label' => 'No. WA',
+        //     'type'  => 'text'
+        // ],
         'foto_ktp' => [
             'label' => 'Foto KTP',
             'type'  => 'file'
         ],
-        'NIK' => [
-            'label' => 'NIK',
-            'type'  => 'text'
-        ],
-        'NPWP' => [
-            'label' => 'NPWP',
-            'type'  => 'text'
-        ],
+        // 'NIK' => [
+        //     'label' => 'NIK',
+        //     'type'  => 'text'
+        // ],
+        // 'NPWP' => [
+        //     'label' => 'NPWP',
+        //     'type'  => 'text'
+        // ],
         'asal_perolehan_dana' => [
             'label' => 'Asal Perolehan Dana',
             'type'  => 'text'
@@ -49,10 +49,10 @@ return [
             'label' => 'Provinsi',
             'type'  => 'text'
         ],
-        'alamat' => [
-            'label' => 'Alamat',
-            'type'  => 'text'
-        ],
+        // 'alamat' => [
+        //     'label' => 'Alamat',
+        //     'type'  => 'text'
+        // ],
         'jumlah_donasi' => [
             'label' => 'Jumlah Donasi',
             'type'  => 'text'
